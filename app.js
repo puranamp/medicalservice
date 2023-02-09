0fd1bc8274fb0479ed7d8a389d890fe4d647f85d
Pranav Puranam <pranav.puranam@gmail.com>
2023-02-08 22:45:35 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let express = require("express");
+let app = express();
+app.use(express.json());
+
+let data = {
+    "bills": []
+}
+
+const validate = (obj) => {
+    let valid = true;
+    const types = {
+        "patientName": "string",
+        "patientAddress": "string",
+        "hospitalName": "string",
+        "dateOfService": "string",
+        "billAmount": "number"
+    }
+    for (const key in types) {
+        if (!(key in obj && typeof obj[key] == types[key])) {
+            valid = false;
+        }
+    }
+    if (valid) return true;
+    return false;
+};
+
+app.get('/items', (req, res) => {
+    res.send(JSON.stringify(data));
+});
+
+app.post('/items', (req, res) => {
+    if (validate(req.body)) {
+        data.bills.push(req.body);
+    }
+    res.send(req.body);
+});
+
+app.listen(3000, () => {
+    console.log(`App listening at http://localhost:${3000}`);
+});
